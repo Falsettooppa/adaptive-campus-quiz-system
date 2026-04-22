74bfbf62fb4b067614aa9c1c1b79f4cb59073336
Abiodun Theophilus Olutoye <abiodunolutoye1912@gmail.com>
2026-04-22 16:17:09 +0100

    feat: refactor database connection handling and add institutional reports template

--- app/db.py
+++ app/db.py
@@ -1,14 +1,17 @@
-from pymongo import MongoClient
 from flask import current_app, g
+from pymongo import MongoClient
+
 
 def get_db():
-    if "db" not in g:
-        client = MongoClient(current_app.config["MONGO_URI"])
-        g.mongo_client = client
-        g.db = client[current_app.config["DATABASE_NAME"]]
+    if "mongo_client" not in g:
+        g.mongo_client = MongoClient(current_app.config["MONGO_URI"])
+        g.db = g.mongo_client[current_app.config["DATABASE_NAME"]]
     return g.db
 
+
 def close_db(e=None):
-    client = g.pop("mongo_client", None)
-    if client is not None:
-        client.close()
+    mongo_client = g.pop("mongo_client", None)
+    g.pop("db", None)
+
+    if mongo_client is not None:
+        mongo_client.close()
